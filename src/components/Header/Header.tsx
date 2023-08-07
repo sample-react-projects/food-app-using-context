5fa4888bbb99ba8e46408b5497161c40f7610139
Saksham Malhotra <saksham.malhotra@openlane.com>
2023-08-08 00:48:11 +0530

    clean up header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Cart from "../Cart/CartButton/CartButton";
 import classes from "./Header.module.scss";
-import { StoreItems } from "../../models/item";
 
-const Header: React.FC<{ cartItems: StoreItems; cartCount: number }> = (
-  props,
-) => {
+const Header: React.FC<{}> = (props) => {
   return (
     <div className={classes.header__container}>
       <span className={classes.header__logo}>Food App</span>
-      <Cart cartItems={props.cartItems} cartCount={props.cartCount}></Cart>
+      <Cart></Cart>
     </div>
   );
 };
