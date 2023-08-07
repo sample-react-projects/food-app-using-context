8f1b54071e19441501bc357d2e2279be0a2414fb
Saksham Malhotra <saksham.malhotra@openlane.com>
2023-08-08 01:24:35 +0530

    Update Products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,21 +1,16 @@
-import React from "react";
-import { StoreItems } from "../../models/item";
-import Item from "../Product/Product";
+import React, { useContext } from "react";
+import Product from "../Product/Product";
+import { CartContext } from "../../store/cart.state";
 
-const Items: React.FC<{
-  items: StoreItems;
-
-  addToCart: (id: string) => void;
-}> = (props) => {
-  const items = Object.values(props.items || {});
+const Products: React.FC<{}> = () => {
+  const cartContext = useContext(CartContext);
+  const items = Object.values(cartContext.items);
 
   return (
     <div className="items">
-      {items?.map((item) => (
-        <Item key={item.id} item={item} addToCart={props.addToCart}></Item>
-      ))}
+      {items?.map((item) => <Product key={item.id} item={item}></Product>)}
     </div>
   );
 };
 
-export default Items;
+export default Products;
--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import classes from "./Root.module.scss";
 import Header from "../Header/Header";
-import Items from "../Products/Products";
+import Products from "../Products/Products";
 
 const App: React.FC<{}> = () => {
   return (
     <div className={classes.app}>
       <Header></Header>
       <div className={classes.container}>
-        <Items></Items>
+        <Products></Products>
       </div>
     </div>
   );
