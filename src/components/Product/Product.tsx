477a53661e33062a2a372b3d0bc3e1f1efa0dc40
Saksham Malhotra <saksham.malhotra@openlane.com>
2023-08-11 02:28:30 +0530

    Determine if product is in stock

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,10 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { StoreItem } from "../../models/item";
 import classes from "./Product.module.scss";
-import { CartContext } from "../../store/cart.state";
+import { CartContext } from "../../store/cart/cart.context";
 
 const Item: React.FC<{ item: StoreItem }> = (props) => {
   const cartContext = useContext(CartContext);
+  const [isOutOfStock, setIsOutOfStock] = useState(false);
+
+  const addToCartHandler = (id: string) => {
+    let itemQuantity = cartContext.items[id].quantity;
+    let cartItemQuantity = cartContext.cartItems[id]?.quantity || 0;
+
+    if (itemQuantity > cartItemQuantity) {
+      cartContext.addItems(id);
+      if (itemQuantity === cartItemQuantity + 1) {
+        setIsOutOfStock(true);
+      }
+    }
+  };
 
   return (
     <li className={classes.item}>
@@ -16,11 +29,10 @@ const Item: React.FC<{ item: StoreItem }> = (props) => {
       <span className={classes.item__price}>₹{props.item.price}</span>
       <button
         className={classes.item__add}
-        onClick={() => {
-          cartContext.addItems(props.item.id);
-        }}
+        onClick={addToCartHandler.bind(null, props.item.id)}
+        disabled={isOutOfStock}
       >
-        Add to Cart
+        {isOutOfStock ? "Out of stock" : "Add to Cart"}
       </button>
     </li>
   );
